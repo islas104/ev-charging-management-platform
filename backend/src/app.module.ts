54b52ee2d4bf6604f0542eedc4c2457ef7f2eb99
Islas Ahmed Nawaz <islas104@gmail.com>
2025-12-19 17:25:19 +0000

    Add request correlation IDs and log propagation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
+import { RequestIdMiddleware } from './common/middleware/request-id.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     LoggerModule.forRoot({
       pinoHttp: {
+        customProps: (req) => ({
+          requestId: req.headers['x-request-id'],
+        }),
         transport: {
           target: 'pino-pretty',
           options: {
@@ -20,4 +24,8 @@ import { AppController } from './app.controller';
   ],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestIdMiddleware).forRoutes('*');
+  }
+}
